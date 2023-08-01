bd6c2f85b62bc870cdb9b4b03ed57a08f9bb985e
Roopa_i <roopaitagi@gmail.com>
2023-08-01 22:06:48 +0530

    Added negative marking, skip option, Different topics and Roles

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Timer;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -14,13 +17,76 @@ public class Main {
         // skip question
         // set timer
 
+        /*long delay = 10;
+        long timer System.currentTimeMillis()+delay;*/
+        Player player = null;
+        boolean playAgain = true;
+        System.out.println("###############################################################");
+        System.out.println("                       Welcome to QuizApp                      ");
+        System.out.println("###############################################################");
+        System.out.println("Choose your option: ");
+        System.out.println("Trainer --> Enter 1.");
+        System.out.println("Player -->  Enter 2.");
+        Scanner roleInput = new Scanner(System.in);
+        int role = roleInput.nextInt();
+        if (role == 1) {
+            //todo : implement code to create the quiz and start the quiz by trainer.
+        } else if (role == 2) {
+            System.out.println("Please enter your name: ");
+            Scanner NameInput = new Scanner(System.in);
+            String name = NameInput.next();
+            player = new Player(name);
 
-        QuestionService service = new QuestionService();
-        //service.displayQuestions();
-        service.playQuiz();
-        service.printScore();
+            System.out.println("##########################################################################");
+            System.out.println("Please read the Quiz rules:  ");
+            System.out.println("**There are total 5 questions and Time limit is 7 minutes.");
+            System.out.println("**You have the option to skip the question if you do not want to answer.");
+            System.out.println("**1 point will be deducted from your score for each wrong answer.");
+            System.out.println("##########################################################################");
+            System.out.println(player.getName() + " Please select the topic for your quiz from the below options:");
+            System.out.println("1.Navin Reddy");
+            System.out.println("2.Core java programming");
+           /* long totalTime=120000;
+            long timer;*/
+            Scanner topicInput = new Scanner(System.in);
+            int topic = topicInput.nextInt();
+            do {
+
+                if (topic == 1) {
+                    QuestionService service = new QuestionService(1);
+                     //timer= System.currentTimeMillis()+totalTime;
+                    //System.out.println("Current time is : "+System.currentTimeMillis());
+                    // while(System.currentTimeMillis() <timer) {
+                         service.playQuiz(player);
+                     //}
+                   // System.out.println("Timeout");
+                } else {
+                    if (topic == 2) {
+                        QuestionService service = new QuestionService(2);
+                        service.playQuiz(player);
+                    }
+                }
+                //service.displayQuestions();
+                // service.printScore();
+               // System.out.println("Current time is : "+System.currentTimeMillis());
+                System.out.println("Do you want to play again?Y/N : ");
+
+                Scanner tryAgain = new Scanner(System.in);
+                String ready = tryAgain.next();
+                if (ready != null && ready.equalsIgnoreCase("Y")) {
+                    playAgain = true;
+                } else {
+                    playAgain = false;
+                }
 
 
+            } while (playAgain);
+            if (null != player) {
+                System.out.println(player.getName() + " Hope you enjoyed the quiz!!! See you again");
+            }
 
+
+        }
     }
+
 }
--- src/QuestionService.java
+++ src/QuestionService.java
@@ -5,6 +5,8 @@ public class QuestionService {
     Question[] questions = new Question[5];
     String selection[] = new String[5];
 
+    int topicNo;
+
     public QuestionService()
     {
         questions[0] = new Question(1, "size of int", "2", "6", "4", "8", "4");
@@ -13,6 +15,23 @@ public class QuestionService {
         questions[3] = new Question(4, "size of long", "2", "6", "4", "8", "8");
         questions[4] = new Question(5, "size of boolean", "1", "2", "4", "8", "1");
 
+    }
+    public QuestionService(int topic){
+
+        if(topic==1){
+            questions[0] = new Question(1, "What is the meaning of Telusko: ", "Get to know ", "Learn", "Knowledge", "Both 1 and 2", "4");
+            questions[1] = new Question(2, "When was Telusko founded", "2", "6", "4", "8", "1");
+            questions[2] = new Question(3, "How many children Navin Reddy has", "Two", "one", "Three", "None", "1");
+            questions[3] = new Question(4, "What is Navin Reddy's fav word", "Aliens", "friends", "Apple", "youtube", "1");
+            questions[4] = new Question(5, "What is Navin Reddy's Wife's name", "Kajol", "Kiran", "Reema", "Deepika", "2");
+        }
+        if(topic==2){
+            questions[0] = new Question(1, "What is the extension of java code files?", ".js", ".txt", ".class", ".java", "4");
+            questions[1] = new Question(2, "Which one of the following is not an access modifier?", "protected", "private", "void", "public", "3");
+            questions[2] = new Question(3, "What is the return type of Constructors", "int", "float", "void", "none of the above", "4");
+            questions[3] = new Question(4, "Which keyword is used by the method to refer to the object that invoked it?", "import", "catch", "abstract", "this", "4");
+            questions[4] = new Question(5, "Which of the following statements are incorrect?", "String is a class", "Strings in java are mutable", "Every String is an object of class String", "None of the above", "2");
+        }
 
     }
 
@@ -24,27 +43,57 @@ public class QuestionService {
 
     }
 
-    public void playQuiz()
+    public void playQuiz(Player player)
     {
         int i=0;
+        int score=0;
+        String answer;
         for(Question q : questions){
+
             System.out.println("Question " + (i+1) + " : ");
-            System.out.println(q.getQuestion());
-            System.out.println(q.getOpt1());
-            System.out.println(q.getOpt2());
-            System.out.println(q.getOpt3());
-            System.out.println(q.getOpt4());
-
-            System.out.println("type the right answer:");
+            System.out.println(q.getId()+"."+q.getQuestion());
+            System.out.println("1. "+q.getOpt1());
+            System.out.println("2. "+q.getOpt2());
+            System.out.println("3. "+q.getOpt3());
+            System.out.println("4. "+q.getOpt4());
+            System.out.println(" ");
+            System.out.println("Do you want to skip this question?Please enter Y/N:  ");
             Scanner sc = new Scanner(System.in);
+            String skipScan= sc.next().trim();
+            if(skipScan.equalsIgnoreCase("Y")){
+                i++;
+                continue;
+            }
+            System.out.println("Enter the right option:");
+           Scanner AnsScanner = new Scanner(System.in);
 
-            selection[i] = sc.nextLine().trim();
+            selection[i] = AnsScanner.nextLine().trim();
+            answer=q.getAnswer();
+            if(answer.equals( selection[i])){
+                System.out.println("Correct Answer");
+                score++;
+            }
+            else{
+                System.out.println("Wrong answer!!!!");
+                score--;
+            }
+
+            System.out.println("Your current score is : " + score+"/"+selection.length);
             i++;
 
         }
+        if(score==selection.length){
+            System.out.println();
+            System.out.println("Congratulations!!!!! "+player.getName());
+            System.out.println("Your Final score is : " + score+"/"+selection.length);
+        }
+        else {
+            System.out.println();
+            System.out.println(player.getName() + " Your Final score is : " + score+"/"+selection.length);
+        }
     }
 
-    public void printScore(){
+    /*public void printScore(){
         int score=0;
         for(String s : selection){
             System.out.println(s);
@@ -55,11 +104,17 @@ public class QuestionService {
             String selAnswer = selection[i];
 
             if(answer.equals(selAnswer)){
+
                 score++;
             }
+            else{
 
+                score--;
+            }
+
+            System.out.println("Your current score is : " + score);
         }
-        System.out.println("result : " + score);
-    }
+        System.out.println("Your Final score is : " + score);
+    }*/
 
 }
